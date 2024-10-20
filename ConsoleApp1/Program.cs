6e14ee72eccc14f5916e679902dd049607c21cff
MichalPedrak <michalpedrak07@gmail.com>
2024-10-20 15:59:33 +0200

    init

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Give water level");
+
+        int waterLevel = int.Parse(Console.ReadLine());
+
+        if (waterLevel > 1000)
+        {
+            Console.WriteLine("Poziom wody jest za wysoki");
+        }
+        else if (waterLevel >= 800 && waterLevel <= 1000)
+        {
+            Console.WriteLine("Poziom wody jest ok");
+        }
+        else
+        {
+            Console.WriteLine("Poziom wody jest za niski1");
+        }
+        
+        
+        
+        
+    }
+}
